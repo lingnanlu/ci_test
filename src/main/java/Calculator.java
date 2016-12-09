2695a25baf8c59fab80b91838de21d99fa523a2a
ht <lingnanlu@gmail.com>
2016-12-09 17:21:35 +0800

    init

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,9 @@
+/**
+ * Created by rico on 2016/12/9.
+ */
+public class Calculator  {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by rico on 2016/12/9.
+ */
+public class CalculatorTest {
+
+    Calculator calculator;
+    @Before
+    public void setUp() throws Exception {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void add() throws Exception {
+        assertEquals(calculator.add(1, 3), 4);
+    }
+
+}
